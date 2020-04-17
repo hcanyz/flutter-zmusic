b09c82a12c5bc7a739972f71e7141a51970769c5
yan.kong <hcanyz@163.com>
2020-04-17 15:32:54 +0800

    [api 调测] netease api v4 categoryPlayList highqualityPlayList params

--- lib/app_home/data/music_api.dart
+++ lib/app_home/data/music_api.dart
@@ -1,7 +1,9 @@
 import 'package:zmusic/app_home/bean/bean.dart';
 
 abstract class MusicApi {
-  Future<HighqualityPlayListWrap> highqualityPlayList();
+  Future<HighqualityPlayListWrap> highqualityPlayList(int page,
+      {int limit = 30});
 
-  Future<CategoryPlayListWrap> categoryPlayList();
+  Future<CategoryPlayListWrap> categoryPlayList(String categoryId, int page,
+      {int limit = 30});
 }
