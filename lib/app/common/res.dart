473426163a1a8e792cbc8c9d7e400d2b3707ed57
yan.kong <hcanyz@163.com>
2020-05-16 18:02:26 +0800

    [future] login页面ui v1

--- lib/app/common/res.dart
+++ lib/app/common/res.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
+const Color zmusic_secondary_color = Color(0xffe3584e);
+
 const Color zmusic_backgroundColor = Color(0xffdb2c1f);
 
 String joinImageAssetPath(String app, String image) {
+  if (app == null || app.isEmpty) {
+    return 'assets/images/$image';
+  }
   return 'lib/app/$app/assets/images/$image';
 }
--- lib/app/login/main.dart
+++ lib/app/login/main.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:zmusic/app/common/res.dart';
 
 class LoginMain extends StatefulWidget {
   @override
@@ -11,13 +10,111 @@ class _LoginMainState extends State<LoginMain> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 2), () {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('login')),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: ConstrainedBox(
+        constraints: BoxConstraints.expand(),
+        child: Stack(alignment: Alignment.topCenter, children: [
+          Positioned(
+            top: 216 - MediaQuery.of(context).padding.top,
+            child: Image.asset(
+              joinImageAssetPath('', 'icon_logo_circle.png'),
+              width: 72,
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            child: Column(
+              children: [
+                MaterialButton(
+                  child: Text(
+                    '手机号登录',
+                    style:
+                        TextStyle(color: zmusic_secondary_color, fontSize: 16),
+                  ),
+                  minWidth: 260,
+                  height: 39,
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  elevation: 0,
+                  onPressed: () {},
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: MaterialButton(
+                    child: Text(
+                      '立即体验',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    minWidth: 260,
+                    height: 39,
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(color: zmusic_secondary_color),
+                        borderRadius: BorderRadius.circular(20.0)),
+                    elevation: 0,
+                    onPressed: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 27.0),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 32.0),
+                        child: Image.asset(
+                          joinImageAssetPath('', 'icon_logo_circle.png'),
+                          width: 31,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 32.0),
+                        child: Image.asset(
+                          joinImageAssetPath('', 'icon_logo_circle.png'),
+                          width: 31,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 32.0),
+                        child: Image.asset(
+                          joinImageAssetPath('', 'icon_logo_circle.png'),
+                          width: 31,
+                        ),
+                      ),
+                      Image.asset(
+                        joinImageAssetPath('', 'icon_logo_circle.png'),
+                        width: 31,
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        value: true,
+                        activeColor: zmusic_secondary_color,
+                        onChanged: (v) {},
+                      ),
+                      Text(
+                        '同意《用户协议》《隐私政策》《儿童隐私政策》',
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
