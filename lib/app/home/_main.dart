b4615e85d5bac4502cdf456a70a5dc5e67aa650b
yan.kong <hcanyz@163.com>
2020-07-01 18:02:57 +0800

    feat(home-main): 首页ui v1 & AutoTextSizeTabBar

--- lib/app/home/_main.dart
+++ lib/app/home/_main.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:zmusic/common/res.dart';
+import 'package:zmusic/widget/autotextsize_tabbar.dart';
 
 class HomeMain extends StatefulWidget {
   @override
   _HomeMainState createState() => _HomeMainState();
 }
 
-class _HomeMainState extends State<HomeMain> {
+class _HomeMainState extends State<HomeMain> with TickerProviderStateMixin {
+  final List<String> _tabValues = [
+    '我的',
+    '发现',
+    '云村',
+    '视频',
+  ];
+
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: _tabValues.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Container(),
+      drawer: Drawer(),
+      appBar: AppBar(
+        flexibleSpace: FlexibleSpaceBar(
+          centerTitle: true,
+          titlePadding: EdgeInsets.only(bottom: 3),
+          title: AutoTextSizeTabBar(
+            tabBarTexts: _tabValues,
+            controller: _controller,
+            defaultTextStyle:
+                TextStyle(fontSize: 15, color: color_text_secondary),
+            selectTextStyle: TextStyle(fontSize: 18, color: color_text_primary),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: _tabValues.map((f) {
+          return Center(
+            child: Text(f),
+          );
+        }).toList(),
+      ),
     );
   }
 }
---
+++ lib/widget/autotextsize_tabbar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AutoTextSizeTabBar extends StatefulWidget {
+  AutoTextSizeTabBar(
+      {this.tabBarTexts,
+      this.controller,
+      this.defaultTextStyle,
+      this.selectTextStyle});
+
+  final List<String> tabBarTexts;
+  final TabController controller;
+
+  final TextStyle defaultTextStyle;
+  final TextStyle selectTextStyle;
+
+  @override
+  _AutoTextSizeTabBarState createState() => _AutoTextSizeTabBarState();
+}
+
+class _AutoTextSizeTabBarState extends State<AutoTextSizeTabBar> {
+  int _curIndex;
+  int _nextIndex;
+  double _t;
+  TextStyle _textStyleCur;
+  TextStyle _textStyleNext;
+
+  @override
+  void initState() {
+    super.initState();
+    _textStyleNext = widget.defaultTextStyle;
+    _textStyleCur = widget.selectTextStyle;
+
+    widget.controller.animation.addListener(() {
+      _curIndex = widget.controller.animation.value.floor();
+      _nextIndex = widget.controller.animation.value.round();
+      var value = (widget.controller.animation.value * 10).floor() / 10;
+      value = value - value.floor();
+      print(value);
+      if (_t != value) {
+        setState(() {
+          _textStyleCur = TextStyle.lerp(
+              widget.defaultTextStyle, widget.selectTextStyle, 1 - value);
+          _textStyleNext = TextStyle.lerp(
+              widget.defaultTextStyle, widget.selectTextStyle, value);
+        });
+        _t = value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      tabs: widget.tabBarTexts.asMap().entries.map((e) {
+        return Container(
+            width: 51,
+            height: 48,
+            alignment: Alignment.center,
+            child: Text(e.value,
+                textAlign: TextAlign.center,
+                style: e.key == _curIndex
+                    ? _textStyleCur
+                    : (e.key == _nextIndex
+                        ? _textStyleNext
+                        : widget.defaultTextStyle)));
+      }).toList(),
+      labelPadding: EdgeInsets.only(),
+      indicator: BoxDecoration(),
+      controller: widget.controller,
+      isScrollable: true,
+    );
+  }
+}
